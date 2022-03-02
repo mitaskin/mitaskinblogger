8b182ab42072a55040b32328be49b99707cf7509
mitaskin <mi.taskin027@gmail.com>
2022-03-02 16:44:52 +0300

    main

--- client/src/components/AddPostForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AddPostForm = () => {
-  return (
-    <div>AddPostForm</div>
-  )
-}
-
-export default AddPostForm
--- client/src/layouts/MainLayout.js
+++ client/src/layouts/MainLayout.js
@@ -1,61 +1,22 @@
 import React from 'react'
-import { CssBaseline, Container, Grid, AppBar, Toolbar, Typography, Button, IconButton, makeStyles } from "@material-ui/core";
-import { useState } from 'react';
+import PostsList from '../components/PostsList';
+import Header from '../components/Header.jsx';
 
-
-const useStyle = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-    container: {
-        marginTop: theme.spacing(2),
-    },
-}));
 
 
 const MainLayout = (props) => {
 
-    const classes = useStyle();
-    const [open, setOpen] = useState(false);
-
     return (
         <>
-            <AppBar position='static' color='inherit' elevation={0}>
-                <Toolbar>
-                    
-                    <IconButton edge="start" className={classes.container} color="inherit" />
-                    
-                    <Typography variant='h6' color="secondary" className={classes.title}>
-                        <a href="http://localhost:3000/posts">Blogify</a>
-                    </Typography>
-                    
-                    <Button color='primary' variant='outlined'  onClick={() => setOpen(true)}>
-                        Ana Sayfa
-                    </Button>
+            <Header/>
+            
 
-                    <Button color='primary' variant='outlined'  onClick={() => setOpen(true)}>
-                        Hakkımda
-                    </Button>
+            <PostsList/>
 
-                    <Button color='primary' variant='outlined'  onClick={() => setOpen(true)}>
-                        İletişim
-                    </Button>
-
-                    <Button color='primary' variant='outlined'  onClick={() => setOpen(true)}>
-                        Yeni Yazı
-                    </Button>
-                    
-                </Toolbar>
-            </AppBar>
             <div>{props.children}</div>
         </>
     )
+    
 }
 
 export default MainLayout
